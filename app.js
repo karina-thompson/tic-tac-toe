6dd7abe9d204206f85f7bb5a1a80d79529268ac1
Karina Thompson <klthompsn8@gmail.com>
2016-03-31 10:44:12 +1100

    updated css styling

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-var pretzel = '<img src=pretzel.png>';
-var donut = '<img src=donut.png>';
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('#turn').innerHTML = pretzel + "'s turn";
-})
-
+//defining variables and the player function to switch turns
 var ticTacToe = {
+  pretzel: '<img src=pretzel.png>',
+  donut: '<img src=donut.png>',
+  board: [null,null,null,null,null,null,null,null,null],
   turnCount: 1,
   player: function() {
     ticTacToe.turnCount = ticTacToe.turnCount + 1;
@@ -18,82 +16,86 @@ var ticTacToe = {
   }
 }
 
-var board = [null,null,null,null,null,null,null,null,null];
-
-
 var checkWinner = function(player) {
+  //horizontal line wins
   for (var i = 0;i<=8;i+=3) { 
-    if (board[i] === player && board[i+1] === player && board[i+2] === player) {
+    if (ticTacToe.board[i] === player && ticTacToe.board[i+1] === player && ticTacToe.board[i+2] === player) {
     return true;
     }
   }
+  //vertical line wins
   for (var j = 0;j<=2;j++) {
-    if (board[j] === player && board[j+3] === player && board[j+6] === player) {
+    if (ticTacToe.board[j] === player && ticTacToe.board[j+3] === player && ticTacToe.board[j+6] === player) {
       return true;
     }
   }
-  if (board[0] === player && board[4] === player && board[8] === player) {
+  //diagonal line wins
+  if (ticTacToe.board[0] === player && ticTacToe.board[4] === player && ticTacToe.board[8] === player) {
       return true;
     }
-  if (board[2] === player && board[4] === player && board[6] === player) {
+  if (ticTacToe.board[2] === player && ticTacToe.board[4] === player && ticTacToe.board[6] === player) {
       return true;
     }
-  if (board.indexOf(null) === -1) {
+  //call it a tie when all squares on the board filled
+  if ((ticTacToe.board).indexOf(null) === -1) {
     return "tie";
   }
   return false;
 }
 
- 
+//Initial turn for pretzel
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#info').innerHTML = ticTacToe.pretzel + " plays first";
+})
 
 
 var gameBoard = document.querySelector('#board');
-var turn = document.querySelector('#turn');
+var info = document.querySelector('#info');
 
-
+//when board is clicked
 gameBoard.addEventListener('click', function(event) {
-  var winner = document.querySelector('#winner');
   if (event.target.className === '') { //Is it a valid move?
     var player = ticTacToe.player();
     if (player === 'O') {
       event.target.className = 'donut';
-      turn.innerHTML = (pretzel + "'s turn");
+      info.innerHTML = (ticTacToe.pretzel + "'s turn");
     }
     else {
        event.target.className = 'pretzel';
-       turn.innerHTML = (donut +"'s turn"); 
+       info.innerHTML = (ticTacToe.donut +"'s turn"); 
     }
   }
-  if (board[event.target.id] === null) {
-    board[event.target.id] = player;
+  if (ticTacToe.board[event.target.id] === null) {
+    ticTacToe.board[event.target.id] = player;
   }
   if (checkWinner(player) === true) {
     if (player === 'O') {
-      return winner.innerHTML = donut + " wins!";
+      return info.innerHTML = ticTacToe.donut + " WINS!";
     } else {
-      return winner.innerHTML = pretzel + " wins!";
+      return info.innerHTML = ticTacToe.pretzel + " WINS!";
     } 
   }
   if (checkWinner(player) === "tie") {
-    winner.innerHTML = "It's a tie!";
+    info.innerHTML = ticTacToe.pretzel + " It's a tie! " + ticTacToe.donut;
   }
 })
 
 
-
-document.querySelector('#reset').addEventListener('click', function() {
+//Reset board button
+document.querySelector('#resetBtn').addEventListener('click', function() {
   for (var i =0;i<gameBoard.children.length;i++) {
     gameBoard.children[i].className = '';
-    board[i] = null;
+    ticTacToe.board[i] = null;
   }
-  winner.innerHTML = '';
   ticTacToe.turnCount = 1;
-  turn.innerHTML = pretzel + "'s turn";
+  info.innerHTML = ticTacToe.donut + " plays first";
 })
 
 
 
 //Add a win count  and increment with each win
-//stop being able to click when someone has won? remove function outside of eventListener and use removeEventListener?
-
+//change who starts each game
+//stop being able to click when someone has won? remove function outside of eventListener and use removeEventListener? have start and end game functions perhaps?
+//local storage??
+//readme file
 
